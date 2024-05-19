ead0b1da4d5d5f821b6015f360802c30daa35e66
ShadowMonster99 <81448108+ShadowMonster99@users.noreply.github.com>
2024-05-18 22:34:15 -0300

    chore(bug): ignore unresolved mods

--- src/transpiler/rollup.config.js
+++ src/transpiler/rollup.config.js
@@ -8,5 +8,21 @@ export default {
   output: {
     file: '../../linker/index.js'
   },
-  plugins: [commonjs(), typescript(), json()]
+  plugins: [commonjs(), typescript(), json()],
+  external: [
+    "chalk", 
+    "path", 
+    "url", 
+    "fs/promises", 
+    "fs", 
+    "rollup", 
+    "@rollup/plugin-json", 
+    "@rollup/plugin-commonjs", 
+    "@rollup/plugin-replace", 
+    "rollup-plugin-typescript2", 
+    "@rollup/plugin-node-resolve",
+    "rollup-plugin-import-css",
+    "@rollup/plugin-terser",
+    "perf_hooks",
+  ]
 };
