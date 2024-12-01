cd0fac6b4aacf6b9269f9e1bc9dbb83d3d7e2a99
shadow <81448108+shdwmtr@users.noreply.github.com>
2024-12-01 01:10:27 -0400

    feat: Add auto version bump

---
+++ script/bump.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the new version passed as an argument
+NEW_VERSION=$1
+
+# Check if version argument is provided
+if [ -z "$NEW_VERSION" ]; then
+  echo "Error: No version provided"
+  exit 1
+fi
+
+# Update the version in package.json
+jq --arg version "$NEW_VERSION" '.version = $version' ./api/package.json > temp.json && mv temp.json ./api/package.json
+jq --arg version "$NEW_VERSION" '.version = $version' ./client/package.json > temp.json && mv temp.json ./client/package.json
+jq --arg version "$NEW_VERSION" '.version = $version' ./webkit/package.json > temp.json && mv temp.json ./webkit/package.json
+
+echo "Version updated to $NEW_VERSION in package.json"
