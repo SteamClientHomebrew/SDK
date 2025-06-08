ab93dad51e000537e087ece357ef08d91795eca7
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-06-08 00:33:49 -0300

    chore: Add define plugin

--- typescript-packages/client/src/index.ts
+++ typescript-packages/client/src/index.ts
@@ -16,3 +16,21 @@ import Toaster from './hooks/toaster-hook';
 export const errorBoundaryHook: ErrorBoundaryHook = new ErrorBoundaryHook();
 export const routerHook: RouterHook = new RouterHook();
 export const toaster: Toaster = new Toaster();
+
+export interface Plugin {
+	version?: string;
+	icon: JSX.Element;
+	content?: JSX.Element;
+	onDismount?(): void;
+	alwaysRender?: boolean;
+	titleView?: JSX.Element;
+}
+
+export type DefinePluginFn = () => Plugin;
+
+export const definePlugin = (fn: DefinePluginFn): DefinePluginFn => {
+	return (...args) => {
+		// TODO: Maybe wrap this
+		return fn(...args);
+	};
+};
